020bdc5921a0ae115ee2d2416660d485f2151f2f
Julien <julbenn@outlook.fr>
2026-05-04 21:55:46 +0200

    fix smtp amen

--- backend/server.js
+++ backend/server.js
@@ -48,8 +48,8 @@ const transporter = nodemailer.createTransport({
   secure: false,
   requireTLS: true,
   auth: {
-    user: EMAIL_USER,
-    pass: EMAIL_PASS
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   }
 });
 
