ec3a3902747f0512bf357ee4a94e38ff72d92401
Julien <julbenn@outlook.fr>
2026-05-30 16:51:31 +0200

    increase upload limit

--- backend/server.js
+++ backend/server.js
@@ -61,7 +61,17 @@ app.use(cors({
 
 app.set("trust proxy", 1);
 
-app.use(express.json());
+app.use(
+  express.json({
+    limit:"20mb"
+  })
+);
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(
   helmet({
